160e073c3f56613dcd327b7bc1e09236dd488165
keepeye <carlton.cheng@foxmail>
2015-05-07 21:51:56 +0800

    init

---
+++ src/Adapter/File.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * File.php
+ * @author: keepeye <carlton.cheng@foxmail.com>
+ */
+
+namespace Keepeye\Cache\Adapter;
+
+
+class File {
+
+}
---
+++ src/Cache.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Cache.php
+ * @author: keepeye <carlton.cheng@foxmail.com>
+ */
+
+namespace Keepeye\Cache;
+
+
+interface Cache {
+
+}
